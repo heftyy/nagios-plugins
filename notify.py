8508ecdc5058033c436ca072792969d64e54c774
masakra <pj@gawex.pl>
2014-09-25 15:38:31 +0200

    hello nagios plugins

---
+++ notify.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+
+def main(argv):
+    print 'Number of arguments:', len(sys.argv), 'arguments.'
+    print 'Argument List:', str(sys.argv)
+    print '---------------------------------------------------------------'
+    return 0
+
+if __name__ == '__main__':
+    pid = str(os.getpid())
+    sys.exit(main(sys.argv))
