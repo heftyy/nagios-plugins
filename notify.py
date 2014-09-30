b6f8db18925c546794aa1f839ea32ea641c9a215
szn <sznowik@gawex.pl>
2014-09-30 11:31:40 +0200

    small fixes to arguments and config

--- notify.py
+++ notify.py
@@ -8,21 +8,24 @@ _log = logging.getLogger('notifyPlugin')
 
 
 class NotifyPlugin(NagiosPlugin):
+
     def run(self, options, host):
         print('host address %s' % host.address)
         print('state %s' % host.state)
 
         data = {}
-        data['node_id'] = host.node_id
+        data['nodeId'] = host.node_id
         data['ip'] = host.address
         data['state'] = host.state
+        data['output'] = host.output
 
         resp = requests.post(self.config.api_notification_url, params=data)
 
         print resp
 
     def add_arguments(self, parser):
-        parser.add_argument('--type', action="store", dest="notification_type", help="Notification type")
+        parser.add_argument(
+            '--type', action="store", dest="notification_type", help="Notification type")
 
 if __name__ == '__main__':
     NotifyPlugin().start()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name = "appname",
+    version = "100",
+    description = "yadda yadda",
+    author = "myself and I",
+    author_email = "email@someplace.com",
+    url = "whatever",
+    #Name the folder where your packages live:
+    #(If you have other packages (dirs) or modules (py files) then
+    #put them into the package directory - they will be found
+    #recursively.)
+    packages = ['plugins'],
+    long_description = """Really long text here.""",
+    #
+    #This next part it for the Cheese Shop, look a little down the page.
+    #classifiers = []
+    scripts=['plugins/notify.py'],
+    entry_points = {
+        'console_scripts': [
+            'notify = plugins.notify:notify',
+        ]
+    }, requires=['requests']
+) 
