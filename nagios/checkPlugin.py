360a2c57554d0f8b60ae1c065ad94a81d6074607
szn <sznowik@gawex.pl>
2015-05-08 10:15:33 +0200

    using value_to_int on return value because python enum is weird

--- nagios/checkPlugin.py
+++ nagios/checkPlugin.py
@@ -155,7 +155,7 @@ class CheckPlugin(NagiosPlugin):
 
         if self.snmp_requester.init_connection():
             script_return_value = self.check(settings)
-            sys.exit(int(script_return_value))
+            sys.exit(NagiosReturnValues.value_to_int(script_return_value))
 
         print "Error, snmp connection failed."
 
