c1e6cb54aa0bd25b987b4261e3ac22292c8fd86e
masakra <pj@gawex.pl>
2015-05-14 12:49:24 +0200

    hello nagios plugins ?!

--- notify.py
+++ notify.py
@@ -8,8 +8,6 @@ def main(argv):
     print 'Number of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
     print '---------------------------------------------------------------'
-    print '---------------------------------------------------------------'
-    print '---------------------------------------------------------------'
     return 0
 
 if __name__ == '__main__':
