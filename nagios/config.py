a9319f8751de182f2f45d7ea96f3b6430c10f671
szn <sznowik@gawex.pl>
2015-05-15 11:03:55 +0200

    updated the config to only use cog

--- nagios/config.py
+++ nagios/config.py
@@ -14,16 +14,9 @@ class PluginConfig:
         config.readfp(f)
         self.cog_ip = config.get('COG', 'ip')
         self.cog_port = config.get('COG', 'port')
-        self.cog_notification_route = config.get('COG', 'notification_route')
 
+        self.cog_notification_route = config.get('COG', 'notification_route')
         self.cog_notification_url = "http://%s:%s/%s" % (self.cog_ip, self.cog_port, self.cog_notification_route)
 
-        self.api_ip = config.get('API', 'ip')
-        self.api_port = config.get('API', 'port')
-        self.api_nagios_settings_route = config.get('API', 'nagios_settings_route')
-
-        self.api_nagios_settings_url = "http://%s:%s/%s" % (self.api_ip, self.api_port, self.api_nagios_settings_route)
-
-        # print "urls:"
-        # print self.cog_notification_url
-        # print self.api_nagios_settings_url
+        self.cog_nagios_settings_route = config.get('COG', 'nagios_settings_route')
+        self.cog_nagios_settings_url = "http://%s:%s/%s" % (self.cog_ip, self.cog_port, self.cog_nagios_settings_route)
