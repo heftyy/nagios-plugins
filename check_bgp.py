b5bcc5e9b39f04c1519e95166b87875e2684d879
szn <sznowik@gawex.pl>
2016-09-21 13:28:00 +0200

    fixed bgp to check actual prefix count

--- check_bgp.py
+++ check_bgp.py
@@ -6,7 +6,10 @@ from nagios.checkPlugin import CheckPlugin
 from nagios.nagiosReturnValues import NagiosReturnValues
 
 
+BGP_IDENT_OID = '1.3.6.1.4.1.2636.5.1.1.2.1.1.1.14.0.1'
+BGP_PREFIX_COUNT_OID = '1.3.6.1.4.1.2636.5.1.1.2.6.2.1.7'
 BGP_OID = '1.3.6.1.2.1.15.3.1'
+BGP_PEER_LOCAL_ADDRESS = 5
 BGP_REMOTE_ADDRESS_END = 7
 BGP_REMOTE_PORT_END = 8
 BGP_IN_MESSAGES_END = 12
@@ -14,11 +17,15 @@ BGP_OUT_MESSAGES_END = 13
 
 
 class BgpSession():
-    def __init__(self, remote_address=None, remote_port=None, in_messages=None, out_messages=None):
+    def __init__(self, remote_address=None, remote_port=None, in_messages=None, out_messages=None,
+                 local_address=None, index=None, prefix_count=None):
         self.remote_address = remote_address
         self.remote_port = remote_port
         self.in_messages = in_messages
         self.out_messages = out_messages
+        self.local_address = local_address
+        self.index = index
+        self.prefix_count = prefix_count
 
 
 class CheckBGP(CheckPlugin):
@@ -47,6 +54,16 @@ class CheckBGP(CheckPlugin):
                 if oid[len(request_bgp_oid)] == BGP_REMOTE_PORT_END:
                     bgp_dict[ident].remote_port = value
 
+                if oid[len(request_bgp_oid)] == BGP_PEER_LOCAL_ADDRESS:
+                    bgp_dict[ident].local_address = value
+
+                    request_bgp_ident_oid = ObjectName('%s.%s.1.%s' % (BGP_IDENT_OID, value.prettyPrint(), ident))
+                    bgp_ident = self.snmp_requester.do_get(request_bgp_ident_oid)
+                    bgp_dict[ident].index = int(bgp_ident[request_bgp_ident_oid])
+                    request_bgp_prefix_count_oid = ObjectName('%s.%d.1.1' % (BGP_PREFIX_COUNT_OID, bgp_dict[ident].index))
+                    bgp_prefix_count = self.snmp_requester.do_get(request_bgp_prefix_count_oid)
+                    bgp_dict[ident].prefix_count = int(bgp_prefix_count[request_bgp_prefix_count_oid])
+
         else:
             raise ValueError("didn't get values from the device for oid %s" % request_bgp_oid)
 
@@ -63,10 +80,10 @@ class CheckBGP(CheckPlugin):
             if 'remote_ip' in bgp:
                 ip = bgp['remote_ip']
                 if ip in bgp_data:
-                    in_messages = bgp_data[ip].in_messages
+                    prefix_count = bgp_data[ip].prefix_count
 
-                    status = self.validate_value_gt(in_messages, bgp)
-                    print "bgp: %s" % in_messages
+                    status = self.validate_value_gt(prefix_count, bgp)
+                    print "bgp: %s" % prefix_count
                     status_list.append(status)
                 else:
                     status_list.append(NagiosReturnValues.state_unknown)
